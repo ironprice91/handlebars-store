c01534e8004a5078a5efc5a016d7596021a2247e
Michael Canlas <michael.canlas428@gmail.com>
2014-10-27 17:26:54 -0600

    dom adding and removing to wishlist items

--- main.js
+++ main.js
@@ -4,22 +4,32 @@ $(document).on('ready', function() {
 	var template = Handlebars.compile(templateStore);
 
 	for (var i = 0; i < productsData.productsList.length; i++) {
-	  console.log(productsData.productsList[i]);
+	  // console.log(productsData.productsList[i]);
 	  var outputHTML = template(productsData.productsList[i]);
 	  $('#product-container').append(outputHTML);
 	};
 
 	$(document).on('click', '.add', function(){
+		var product = $(this).closest('div');
+		// $('#wishlist').append(product);
+		var productTitle = product.find('h1').text().toString();
+		console.log(productTitle);
+
 		var teplateWishlist = $('#wishlist-items').html();
 		var template2 = Handlebars.compile(teplateWishlist);
+
 		for (var i = 0; i < productsData.productsList.length; i++) {
-		  var outputHTML2 = template2(productsData.productsList[i]);
-		  $('#wishlist').append(outputHTML2);
+		  var item = productsData.productsList[i];
+		  var outputHTML2 = template2(item);
+		  if(productTitle === item.title){
+		  	$('#wishlist').append(outputHTML2); 
+		  }
 		};
 	});	
 
 	$(document).on('click', '.remove', function(){
-		console.log($(this));
+		var wishlistItem = $(this).closest('li');
+		wishlistItem.remove();
 	});
 
 });
